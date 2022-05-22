dc39778eb5c211afc74f5f1d690967713ecf6bba
Dedsec R4 <rafeteminov313@gmail.com>
2022-05-22 12:16:38 +0400

    added new render function in store

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import searchReducer from "../reducers/search";
 import sliderReducer from "../reducers/sliderUsers";
 import postReducer from "../reducers/posts";
 import suggestionReducer from "../reducers/suggestions";
+import {configureStore} from "@reduxjs/toolkit";
 
 const stringMiddleware = () => (next) => (action) => {
     if(typeof action === 'string'){
@@ -15,15 +16,23 @@ const stringMiddleware = () => (next) => (action) => {
     return next(action)
 }
 
-const store = createStore(
+//Very old version
+/*const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())*/
+
+//Redux Version
+/*const store = createStore(
     combineReducers({searchReducer, sliderReducer, postReducer, suggestionReducer}),
     compose(
         applyMiddleware(ReduxThunk, stringMiddleware),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
-)
+)*/
 
-/*const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())*/
-
+//Redux ToolKit Version
+const store = configureStore({
+    reducer: {searchReducer, sliderReducer, postReducer, suggestionReducer},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production'
+})
 
 export default store;
